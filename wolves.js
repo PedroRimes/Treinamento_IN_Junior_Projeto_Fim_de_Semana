3296db9fc96778e7a71db6445f538d0c2990a214
Pedro Coutinho Rimes <pcoutinhorimes@gmail.com>
2021-10-02 15:29:01 -0300

    js lobos

---
+++ wolves.js
@@ -0,0 +1,32 @@
+const wolves = [
+    {
+        name: "lobo branco",
+	    age: 3,
+	    link_image: "image (2).png",
+	    description: "Lobo branco mora na floresta",
+    },
+    {
+        name: "lobo montanha",
+        age: 5,
+        link_image: "lobo_montanha.jpg",
+        description: "Lobo da montanha mora na montanha"
+    },
+    {
+        name: "lobo laranja",
+        age: 10,
+        link_image: "Lobo_laranja.jpg",
+        description: "Lobo da laranja mora na floresta"
+    },
+    {
+        name: "lobo guará",
+        age: 9,
+        link_image: "LOBO_GUARÁ,jpg",
+        description: "Lobo da guará mora no Brasil"
+    }, 
+    {
+        name: "lobo cinza",
+        age: 14,
+        link_image: "image (2).png",
+        description: "Lobo da cinza mora na montanha"
+    },
+]
