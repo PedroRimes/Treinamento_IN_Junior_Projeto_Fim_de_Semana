74e045c3d9df7ac524a59e6cfacf03390f5e8236
Israel Navarro <isra.lemes@gmail.com>
2021-10-03 16:57:58 -0300

    Criando a funcionalidade de adicionar um novo Lobo

--- wolves.js
+++ wolves.js
@@ -1,70 +1,40 @@
-// const wolves = [
-//     {
-//         name: "lobo branco",
-// 	    age: 3,
-// 	    link_image: "image (2).png",
-// 	    description: "Não obstante, o surgimento do comércio virtual faz parte de um processo de gerenciamento do levantamento das variáveis envolvidas. Não obstante, o surgimento do comércio virtual faz parte de um processo de gerenciamento do levantamento das variáveis envolvidas.Não obstante, o surgimento do comércio virtual faz parte de um processo de gerenciamento do levantamento das variáveis envolvidas.Não obstante, o surgimento do comércio virtual faz parte de um processo de gerenciamento do levantamento das variáveis envolvidas.",
-//     },
-//     {
-//         name: "lobo montanha",
-//         age: 5,
-//         link_image: "lobo_montanha.jpg",
-//         description: "Não obstante, o surgimento do comércio virtual faz parte de um processo de gerenciamento do levantamento das variáveis envolvidas. Não obstante, o surgimento do comércio virtual faz parte de um processo de gerenciamento do levantamento das variáveis envolvidas.Não obstante, o surgimento do comércio virtual faz parte de um processo de gerenciamento do levantamento das variáveis envolvidas.Não obstante, o surgimento do comércio virtual faz parte de um processo de gerenciamento do levantamento das variáveis envolvidas."
-//     },
-//     {
-//         name: "lobo laranja",
-//         age: 10,
-//         link_image: "Lobo_laranja.jpg",
-//         description: "Não obstante, o surgimento do comércio virtual faz parte de um processo de gerenciamento do levantamento das variáveis envolvidas. Não obstante, o surgimento do comércio virtual faz parte de um processo de gerenciamento do levantamento das variáveis envolvidas.Não obstante, o surgimento do comércio virtual faz parte de um processo de gerenciamento do levantamento das variáveis envolvidas.Não obstante, o surgimento do comércio virtual faz parte de um processo de gerenciamento do levantamento das variáveis envolvidas."
-//     },
-//     {
-//         name: "lobo guará",
-//         age: 9,
-//         link_image: "LOBO_GUARÁ,jpg",
-//         description: "Não obstante, o surgimento do comércio virtual faz parte de um processo de gerenciamento do levantamento das variáveis envolvidas. Não obstante, o surgimento do comércio virtual faz parte de um processo de gerenciamento do levantamento das variáveis envolvidas.Não obstante, o surgimento do comércio virtual faz parte de um processo de gerenciamento do levantamento das variáveis envolvidas.Não obstante, o surgimento do comércio virtual faz parte de um processo de gerenciamento do levantamento das variáveis envolvidas."
-//     }, 
-//     {
-//         name: "lobo cinza",
-//         age: 14,
-//         link_image: "image (2).png",
-//         description: "Não obstante, o surgimento do comércio virtual faz parte de um processo de gerenciamento do levantamento das variáveis envolvidas. Não obstante, o surgimento do comércio virtual faz parte de um processo de gerenciamento do levantamento das variáveis envolvidas.Não obstante, o surgimento do comércio virtual faz parte de um processo de gerenciamento do levantamento das variáveis envolvidas.Não obstante, o surgimento do comércio virtual faz parte de um processo de gerenciamento do levantamento das variáveis envolvidas."
-//     },
-// ]
-
 const $ = document
-let submit = $.querySelector('#entrada-dados');
-const url = "http://lobinhos.herokuapp.com"
-
-// APARECE O LOBO /////////
-// const apareceLobo = (id,name,age,link_image,description) =>{
-//     new_child.id = "div-group-" + id
-//     new_child.classList.add("div_group")
-//     new_child.innerHTML= `
-//     <div class="imagens-sobrepostas">
-//         <figure class="quadrado-azul"></figure>
-//         <img class="imagem-lobo" src="${link_image}" alt="">
-//     </div>
-//     <div class="descricao-lobo">
-//         <div>
-//             <p class="nome-lobo">${name}</p>
-//             <button type="submit" class="caixaadotar">Adotar</button>
-//         </div>
-//         <div>
-//             <h3 class="idade-lobo">Idade: ${age} anos</h3>
-//             <p class="texto-lobo">${description}</p>
-//         </div>
-//     </div>`
-//     list.appendChild(new_child)
-// }}
+let list = $.querySelector('#entrada-dados');
+let submit = $.querySelector('#botao');
+const url = "http://lobinhos.herokuapp.com/wolves"
 
 // GET /////////////////////////////////////
 
-const getWolves = (fetch(url).then(resp => resp.json()))
+fetch(url).then(resp => resp.json())
+
+// APARECE O LOBO /////////
+const apareceLobo = (id,name,age,link_image,description) =>{
+    let new_child = $.createElement("div")
+    new_child = "div-group-" + id
+    new_child.innerHTML= `
+    <div class="imagens-sobrepostas">
+        <figure class="quadrado-azul"></figure>
+        <img class="imagem-lobo" src="${link_image}" alt="">
+    </div>
+    <div class="descricao-lobo">
+        <div>
+            <p class="nome-lobo">${name}</p>
+            <button type="submit" class="caixaadotar">Adotar</button>
+        </div>
+        <div>
+            <h3 class="idade-lobo">Idade: ${age} anos</h3>
+            <p class="texto-lobo">${description}</p>
+        </div>
+    </div>`
+    new_child.classList.add("div_group")
+    list.appendChild(new_child)
+}
 
 // Adicionar Novo Lobo //////////////////////////////////////////////////////////
 
 submit.addEventListener('click', () =>{
     var name = $.querySelector("#name").value;
+    console.log(name)
     var age = $.querySelector("#age").value;
     var link_image = $.querySelector("#link_image").value;
     var description = $.querySelector("#description").value;
@@ -84,12 +54,9 @@ submit.addEventListener('click', () =>{
         }
         fetch(url,fetchConfig)
         .then(resp => resp.json())
-        .then(wolf => {
-            wolf.forEach(element => {
-                apareceLobo(element => element)
-            });
-        })
-    }
+        .then(element => apareceLobo(element.id, element.name, element.age, element.link_image, element.description))
+        .catch(error => console.warn(error))
 
+    }
 })
 ////////////////////////////////////////////////////////////////////
